f9d8517936960823e73b1832f23f3d3b848a9527
Thomas Yoho <TYohoJr@Gmail.com>
2019-03-28 18:14:29 -0500

    tetsing

--- express.js
+++ express.js
@@ -3,3 +3,30 @@ var app = express();
 var path = require('path')
 require('dotenv').config();
 var bodyParser = require('body-parser');
+
+// app.use(express.static(path.join(__dirname, "build")));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    console.log(`${req.body.request_id} Unique????`)
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+}); 
+
+app.listen(process.env.PORT || 8080, () => {
+    var curPort = process.env.PORT;
+    if (curPort === undefined) {
+        console.log(`listening on localhost://8080`)
+        curPort = "localhost://8080"
+    } else {
+        let d = new Date();
+        console.log(`listening on ${curPort} at ${d}`)
+    }
+})
+
+app.post('/apiTest', (req, res) => {
+    app.get('')
+    res.json({
+        hello:'hello'
+    });
+})
